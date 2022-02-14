cce254edb942e085acc73e5060522586451d28a0
Simon Tulling <simon.tulling99@gmail.com>
2022-02-14 13:23:58 +0100

    Revert "portaudio add"
    This reverts commit ee0d87c451f3c299a2155d3d5d3fb22634304ce5.

--- audio/audioIn_test.go
+++ audio/audioIn_test.go
@@ -1,50 +1,121 @@
 package main
 
 import (
+	"encoding/binary"
+	"fmt"
 	"testing"
 	"time"
 
-	"github.com/gordonklaus/portaudio"
+	"github.com/gen2brain/malgo"
 )
 
-func TestPortautio(t *testing.T) {
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-	e := newEcho(time.Second / 3)
-	defer e.Close()
-	chk(e.Start())
-	time.Sleep(4 * time.Second)
-	chk(e.Stop())
-}
+const (
+	periodSize uint32 = 100
+	delay      int    = 10
+)
 
-type echo struct {
-	*portaudio.Stream
-	buffer []float32
-	i      int
-}
+var (
+	sizeInBytes uint32      = 2
+	queue       chan []byte = make(chan []byte, delay+10)
+)
 
-func newEcho(delay time.Duration) *echo {
-	h, err := portaudio.DefaultHostApi()
-	chk(err)
-	p := portaudio.LowLatencyParameters(h.DefaultInputDevice, h.DefaultOutputDevice)
-	p.Input.Channels = 1
-	p.Output.Channels = 1
-	e := &echo{buffer: make([]float32, int(p.SampleRate*delay.Seconds()))}
-	e.Stream, err = portaudio.OpenStream(p, e.processAudio)
-	chk(err)
-	return e
-}
-
-func (e *echo) processAudio(in, out []float32) {
-	for i := range out {
-		out[i] = .7 * e.buffer[e.i]
-		e.buffer[e.i] = in[i]
-		e.i = (e.i + 1) % len(e.buffer)
+func readBlock(samples []byte, frameCount uint32) {
+	block := make([]int16, frameCount)
+	for i := uint32(0); i < frameCount; i++ {
+		block[i] = int16(binary.LittleEndian.Uint16(samples[i*sizeInBytes*2 : i*sizeInBytes*2+1]))
 	}
 }
 
-func chk(err error) {
+func captureCallback(outputSamples, inputSamples []byte, frameCount uint32) {
+	fmt.Printf("[INFO] Captured %v samples\n", frameCount)
+	copied := make([]byte, len(inputSamples))
+	copy(copied, inputSamples)
+	go readBlock(copied, frameCount)
+	queue <- copied
+
+}
+
+func playbackCallback(outputSamples, inputSamples []byte, frameCount uint32) {
+	data := <-queue
+	if len(data) != 0 {
+		copy(outputSamples, data)
+	} else {
+		copy(outputSamples, make([]byte, len(outputSamples)))
+	}
+}
+
+func initDevice(ctx *malgo.AllocatedContext, deviceType malgo.DeviceType, fun malgo.DataProc) {
+	infos, err := ctx.Devices(deviceType)
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("[INFO] Found %v devices\n", len(infos))
+	for _, info := range infos {
+		fmt.Printf("[INFO] Device: %v\n", info.Name())
+	}
+
+	fmt.Printf("[INFO] Using device %v\n", infos[0].Name())
+
+	full, _ := ctx.DeviceInfo(deviceType, infos[0].ID, malgo.Shared)
+
+	fmt.Printf("[%s] Channels: %d-%d\n", full.Name(), full.MinChannels, full.MaxChannels)
+	fmt.Printf("[%s] SampleRate: %d-%d\n", full.Name(), full.MinSampleRate, full.MaxSampleRate)
+
+	deviceConfig := malgo.DefaultDeviceConfig(deviceType)
+	deviceConfig.Capture.Format = malgo.FormatS16
+	deviceConfig.Capture.Channels = full.MinChannels
+	deviceConfig.Playback.Format = malgo.FormatS16
+	deviceConfig.Playback.Channels = full.MinChannels
+	deviceConfig.SampleRate = full.MaxSampleRate
+	deviceConfig.PeriodSizeInMilliseconds = periodSize
+	deviceConfig.PeriodSizeInFrames = 0
+	deviceConfig.Periods = 1
+	deviceConfig.Alsa.NoMMap = 1
+
+	callbacks := malgo.DeviceCallbacks{
+		Data: fun,
+	}
+	captureDeviceConfig := deviceConfig
+	captureDeviceConfig.DeviceType = deviceType
+	device, err := malgo.InitDevice(ctx.Context, captureDeviceConfig, callbacks)
+	if err != nil {
+		panic(err)
+	}
+
+	err = device.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	if !device.IsStarted() {
+		panic("device not started")
+	}
+}
+
+func RunAudioPipe() {
+	ctx, err := malgo.InitContext([]malgo.Backend{malgo.BackendAlsa}, malgo.ContextConfig{}, func(message string) {
+		fmt.Printf("LOG <%v>\n", message)
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = ctx.Uninit()
+		ctx.Free()
+	}()
+	println("Filling delay buffer")
+	for i := 0; i < delay; i++ {
+		queue <- make([]byte, 0)
+	}
+	println("Initilizing audio capture device")
+	initDevice(ctx, malgo.Capture, captureCallback)
+	println("Initilizing audio playback device")
+	initDevice(ctx, malgo.Playback, playbackCallback)
+
+	time.Sleep(time.Hour * 1000000)
+}
+
+func Test(t *testing.T) {
+	RunAudioPipe()
 }
--- audio/go.mod
+++ audio/go.mod
@@ -5,7 +5,6 @@ go 1.15
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gen2brain/malgo v0.10.35
-	github.com/gordonklaus/portaudio v0.0.0-20200911161147-bb74aa485641
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
