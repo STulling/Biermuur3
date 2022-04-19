a17f68bb2c2e596be20345e4d692fdb9a3559906
AaronvDiepen <aaronvdiepen@gmail.com>
2022-04-19 20:54:13 +0200

    Fix list of effects consisting of only empty objects

---
+++ video/api/effects_lang.go
@@ -0,0 +1,33 @@
+package api
+
+type Effect struct {
+	Title string `json:"title"`
+	Url   string `json:"url"`
+}
+
+var effects = map[string][]Effect{
+	"nl_NL": {
+		{Title: "Wave", Url: "wave"},
+		{Title: "Sparkle", Url: "sparkle"},
+		{Title: "Mond", Url: "mond"},
+		{Title: "Fill", Url: "fill"},
+		{Title: "Diamond", Url: "diamond"},
+		{Title: "Circle", Url: "circle"},
+		{Title: "Bars", Url: "bars"},
+		{Title: "Clear", Url: "clear"},
+		{Title: "Snake", Url: "snake"},
+		{Title: "Clock", Url: "clock"},
+	},
+	"af_ZA": {
+		{Title: "Golf", Url: "wave"},
+		{Title: "Vonkel", Url: "sparkle"},
+		{Title: "Bek", Url: "mond"},
+		{Title: "Vullen", Url: "fill"},
+		{Title: "Diamant", Url: "diamond"},
+		{Title: "Sirkel", Url: "circle"},
+		{Title: "Stafies", Url: "bars"},
+		{Title: "Skoon", Url: "clear"},
+		{Title: "Slang", Url: "snake"},
+		{Title: "Horlosie", Url: "clock"},
+	},
+}
